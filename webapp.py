1442bbc2587142f2569309573fb602176a21f09f
m0xn <moon0xcoder@gmail.com>
2024-05-31 18:26:05 +0200

    Finished adding all the content

--- webapp.py
+++ webapp.py
@@ -1,34 +1,83 @@
 import streamlit as st
 import pandas as pd
-from math import pow
-from numpy import sum
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 
-def double_entry_table(in_df, f_x, f_y):
-    diff_x_vals = list(set(in_df[f_x]))
-    diff_y_vals = list(set(in_df[f_y]))
+def gen_freq_table(df):
+    unique_pairs = df.value_counts().index.to_numpy()
+    frequencies = df.value_counts()\
+                    .to_numpy()\
+                    .reshape(-1, 1)
 
-    distrib_table = pd.DataFrame(index=diff_y_vals,
-                                 columns=diff_x_vals,
-                                 dtype='int16')
+    grades_array = []
+    for pair in unique_pairs:
+        x, y = pair
+        grades_array.append(x)
+        grades_array.append(y)
 
-    distrib_table['Total'] = 0 
+    unique_pairs = np.array([grades_array]).reshape(-1, 2)
+    pair_freq_data = np.concatenate((unique_pairs, frequencies), axis=1)
 
-    for i, x in enumerate(diff_x_vals):
-        for j, y in enumerate(diff_y_vals):
-            distrib_table.iloc[j, i] = len(df.loc[(in_df[f_x] == x) &
-                                                  (in_df[f_y] == y)])
+    return (pd.DataFrame(pair_freq_data, columns=['x_i', 'y_i', 'n_i']),
+            unique_pairs)
 
-    for j, y in enumerate(diff_y_vals):
-        distrib_table.iloc[j, -1] = sum(distrib_table.iloc[j])
 
-    diff_y_vals.append('Total')
-    distrib_table = distrib_table.reindex(diff_y_vals)
+def gen_double_entry_tbl(freq_table, unique_pairs):
+    unique_xs = list(set(unique_pairs[:, 0]))
+    unique_ys = list(set(unique_pairs[:, 1]))
 
-    for i, col in enumerate(distrib_table):
-        distrib_table.iloc[-1, i] = sum(distrib_table[col])
+    print(unique_xs)
 
-    return distrib_table
+    array = np.ndarray((len(unique_ys), len(unique_xs)),
+                       dtype='int8')
+    array.fill(0)
+
+    double_entry_table = pd.DataFrame(array,
+                                      columns=unique_xs,
+                                      index=unique_ys)
+    for x, y in unique_pairs:
+        double_entry_table.loc[y, x] = freq_table.loc[(freq_table['x_i'] == x) &
+                                                      (freq_table['y_i'] == y)]\
+                                                      .iloc[0, -1]
+    double_entry_table['Total'] = 0
+
+    for i in double_entry_table.index:
+        double_entry_table.loc[i, 'Total'] = np.sum(double_entry_table.loc[i])
+
+    new_index = double_entry_table.index.join(pd.Index(['Total']), how='outer')
+    double_entry_table = double_entry_table.reindex(index=new_index,
+                                                    fill_value=0)
+
+    for col in double_entry_table:
+        double_entry_table.loc['Total', col] = double_entry_table[col].sum()
+
+    return double_entry_table
+
+
+def covariance_state(cov_val):
+    if cov_val >= -0.3 and cov_val <= 0.3:
+        return r'\sigma_{xy}\approx 0'
+    elif cov_val < -0.3:
+        return r'\sigma_{xy}\lt 0'
+    else:
+        return r'\sigma_{xy}\gt 0'
+
+
+def corr_coef_state(r2):
+    if r2 <= -0.8 and r2 >= -0.9:
+        return r'r\approx -1'
+    elif r2 == -1:
+        return 'r=-1'
+    elif r2 >= 0.8 and r2 <= 0.9:
+        return r'r\approx 1'
+    elif r2 == 1:
+        return 'r=1'
+    if r2 >= -0.3 and r2 <= 0.3:
+        return r'r\approx 0'
+    else:
+        return r'r\ll 1' if r2 > 0 else r'r\gg -1'
 
 
 # ------------ MAIN PAGE -----------------
@@ -62,7 +111,7 @@ with st.sidebar:
     raw_file = st.file_uploader(label='Conjunto de datos (*.xlsx, *.csv)',
                                 type=['.csv', '.xlsx'])
     if hints_enabled:
-        st.markdown("""
+        st.markdown(r"""
                     *Sube aquí el archivo con tu conjunto de datos, puede ser un
                     fichero de excel (\*.xlsx) o un archivo CSV (\*.csv)*
                     """)
@@ -107,6 +156,7 @@ with st.sidebar:
             y = st.selectbox(label='Variable Y',
                              options=df.columns)
 
+# ----------- FREQ TABLES -----------
 if raw_file and enough_features:
     st.header('Análisis de los datos')
     st.subheader('Tablas de variable bidimensional')
@@ -119,17 +169,21 @@ if raw_file and enough_features:
 
     rel_freq, double_entry = st.columns(2)
 
+    freq_df, unique_pairs = gen_freq_table(df)
+
     with rel_freq:
-        st.markdown('<p style="text-align: center; font-size: 20px;">\
+        st.markdown('<p style="font-size: 20px;">\
                     Tabla de frecuencias relativas</p>',
                     unsafe_allow_html=True)
 
-        st.dataframe(df.value_counts())
+        st.dataframe(freq_df)
 
     with double_entry:
-        st.markdown('<p style="text-align: center; font-size: 20px;">\
+        st.markdown('<p style="font-size: 20px;">\
                     Tabla de doble entrada</p>', unsafe_allow_html=True)
-        st.dataframe(double_entry_table(df, x, y))
+        st.dataframe(gen_double_entry_tbl(freq_df, unique_pairs))
+
+# ----------- STAT MEASURES -----------
 
     st.subheader('Métricas estadísticas')
     st.markdown("""
@@ -144,8 +198,8 @@ if raw_file and enough_features:
     std_x = df[x].std(ddof=0)
     std_y = df[y].std(ddof=0)
 
-    var_x = pow(df[x].std(ddof=0), 2)
-    var_y = pow(df[y].std(ddof=0), 2)
+    var_x = df[x].std(ddof=0)**2
+    var_y = df[y].std(ddof=0)**2
 
     col1, col2 = st.columns(2)
 
@@ -161,9 +215,9 @@ if raw_file and enough_features:
         st.markdown('<h3 style="text-align: center; padding: 0;">\
                      Métricas variable Y</h3>', unsafe_allow_html=True)
 
-        st.latex(r'\Large\bar{x}=' + str(round(mean_y, 3)))
-        st.latex(r'\Large\sigma_x=' + str(round(std_y, 3)))
-        st.latex(r'\Large\sigma^2_x=' + str(round(var_y, 3)))
+        st.latex(r'\Large\bar{y}=' + str(round(mean_y, 3)))
+        st.latex(r'\Large\sigma_y=' + str(round(std_y, 3)))
+        st.latex(r'\Large\sigma^2_y=' + str(round(var_y, 3)))
 
     st.markdown('<h3 style="text-align: center;">\
                  Métricas de X e Y</h3>', unsafe_allow_html=True)
@@ -173,3 +227,103 @@ if raw_file and enough_features:
 
     st.latex(r'\Large\sigma_{xy}=' + str(round(cov_xy, 4)))
     st.latex(r'\Large r=' + str(round(r2, 5)))
+
+# ----------- LINREG -----------
+
+    st.subheader('Diagrama de dispersión')
+    st.markdown("""
+                En este apartado podrás observar la relación entre las métricas
+                de la covarianza y el coeficiente de correlación de Pearson y
+                la disposición de los puntos en la nube de puntos.
+                """)
+
+    st.pyplot(plt.scatter(data=df, x=y, y=x).figure)
+    st.latex(covariance_state(cov_xy) + ';' + corr_coef_state(r2))
+
+# ----------- LINREG -----------
+
+    st.subheader('Regresión Lineal')
+    st.markdown("""
+                En este apartado se muestran los diagramas de dispersión con
+                las respectivas rectas de regresión de una variable sobre la
+                otra.
+                """)
+
+    xy_regr_col, yx_regr_col = st.columns(2)
+
+    with xy_regr_col:
+        st.markdown('<p style="text-align: center; font-size: 20px;"><strong>\
+                    Recta de regresión de y sobre x</strong></p>',
+                    unsafe_allow_html=True)
+
+        xy_model = LinearRegression()
+        xy_model.fit(df[x].to_numpy().reshape(-1, 1), df[y])
+
+        fig, ax = plt.subplots()
+        ax.scatter(data=df, x=x, y=y, c='blue')
+        ax.axline((0, xy_model.intercept_), slope=xy_model.coef_[0], c='blue')
+
+        plt.xlim(df[x].min())
+        plt.ylim(df[y].min())
+
+        st.pyplot(fig)
+        st.latex(r'y=' + str(round(xy_model.coef_[0], 3)) + 'x'
+                 + '+' + str(round(xy_model.intercept_, 2)))
+
+    with yx_regr_col:
+        st.markdown('<p style="text-align: center; font-size: 20px;"><strong>\
+                    Recta de regresión de y sobre x</strong></p>',
+                    unsafe_allow_html=True)
+
+        yx_model = LinearRegression()
+        yx_model.fit(df[y].to_numpy().reshape(-1, 1), df[x])
+
+        fig, ax = plt.subplots()
+        ax.scatter(data=df, x=y, y=x, c='red')
+        ax.axline((0, yx_model.intercept_), slope=yx_model.coef_[0], c='red')
+
+        plt.xlim(df[y].min())
+        plt.ylim(df[x].min())
+
+        st.pyplot(fig)
+
+        st.latex(r'x=' + str(round(yx_model.coef_[0], 3)) + 'y'
+                 + '+' + str(round(yx_model.intercept_, 2)))
+
+# ----------- LINREG TESTING -----------
+
+    st.subheader('Aplicación de las rectas de regresión')
+    st.markdown("""
+                Finalmente, en este apartado puedes introducir valores
+                para cada una de las variables, de modo que se realice
+                una estimación utilizando la ecuación de las rectas de
+                regresión calculadas:
+                """)
+
+    x_pred_col, y_pred_col = st.columns(2)
+
+    with x_pred_col:
+        x_val = st.number_input(label='Valor X',
+                                min_value=0.00,
+                                max_value=10.00,
+                                step=1.00)
+
+        y_pred = xy_model.predict([[x_val]])
+        st.latex(r'x=' + str(x_val))
+
+        st.latex(r'y=' + str(round(xy_model.coef_[0], 3)) + r'\cdot'
+                 + str(x_val) + '+' + str(round(xy_model.intercept_, 2)) + '='
+                 + str(round(y_pred[0], 2)))
+
+    with y_pred_col:
+        y_val = st.number_input(label='Valor Y',
+                                min_value=0.00,
+                                max_value=10.00,
+                                step=1.00)
+
+        x_pred = yx_model.predict([[y_val]])
+        st.latex(r'y=' + str(y_val))
+
+        st.latex(r'x=' + str(round(yx_model.coef_[0], 3)) + r'\cdot'
+                 + str(y_val) + '+' + str(round(yx_model.intercept_, 2)) + '='
+                 + str(round(x_pred[0], 2)))
